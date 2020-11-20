81bf49e5a385e66afab732bb64103d080c8fcd1f
santiagojordan <sjordan@udesa.edu.ar>
2020-11-20 01:31:30 -0300

    js

---
+++ js/genres.js
@@ -0,0 +1,18 @@
+var apikey='811c43dae475e925f89e17499fd11f5c'
+
+window.addEventListener("load" function(){}
+
+fetch(`https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=${apikey} &language=en-US&page=1`)
+.then(function(response){
+return response.json();
+})
+.then(function(data){
+console.log(data);
+})
+.catch(function(error){
+console.log('El error fué: ' + error);
+})
+
+
+
+})
