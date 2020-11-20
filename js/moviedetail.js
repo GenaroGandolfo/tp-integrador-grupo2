e50bf621c20246239892a5391df9bdcf2ebad24b
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-19 22:56:35 -0300

    js

---
+++ js/moviedetail.js
@@ -0,0 +1,20 @@
+window.addEventListener("load" function(){}
+
+fetch('https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=811c43dae475e925f89e17499fd11f5c&language=en-US&page=1')
+.then(function(response){
+return response.json();
+})
+.then(function(data){
+console.log(data);
+})
+.catch(function(error){
+console.log('El error fué: ' + error);
+})
+
+
+
+})
+
+
+
+// 811c43dae475e925f89e17499fd11f5c
