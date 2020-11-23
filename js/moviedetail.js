ee3d1871b609b70d16fb3a77c6c11e76620942f7
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-23 16:50:29 -0300

    .

--- js/moviedetail.js
+++ js/moviedetail.js
@@ -1,6 +1,5 @@
-var apikey='811c43dae475e925f89e17499fd11f5c'
-
 window.addEventListener("load",function(){
+var apikey='811c43dae475e925f89e17499fd11f5c'
 
 fetch(``)
 .then(function(response){
@@ -10,6 +9,8 @@ return response.json();
    for (let index = 0; index < data.results.length; index++) {
        const element = data.results [index];
        
+
+    
    }
    console.log(data);
 })
