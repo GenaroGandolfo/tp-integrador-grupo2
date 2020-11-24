5f4ef501ee3c173915bb34ce58d18a56eaa01578
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-23 23:48:21 -0300

    fetch populares

--- js/index.js
+++ js/index.js
@@ -1,23 +1,44 @@
-window.addEventListener("load", function(){
+window.addEventListener('load', function(){
 
-    var apiKey='811c43dae475e925f89e17499fd11f5c'
+    var apiKey  ='811c43dae475e925f89e17499fd11f5c'
+    var imageneslink ='https://image.tmdb.org/t/p/w500'
 
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=811c43dae475e925f89e17499fd11f5c&language=en-US&page=1`)
 	.then(function(response){
 	return response.json();
 })
 	.then(function(data){
-    console.log(data);
+        console.log(data)
+
+    
+    var populares = document.querySelector ("#pop");
+   
+    
     for (let index = 0; index < data.results.length; index++) {
-        var results = data.results[index]; 
+         var results = data.results[index];
         
-    populares.innerHTML+=
+    }
 
-    `<li>
-    <a href="movieDetails.html"> <div><img src="imagenes/soncomoninos.jpg" alt="Son como niños"> </div></a> 
-    </li>`
 
-}
+        populares.innerHTML+= 
+        `
+       
+       
+            <li>
+                <a href="movieDetails.html"> 
+                <div> <img src="${imageneslink}${results.poster_path}" alt=""> </div> 
+                </a> 
+            </li>  
+     
+        
+        
+        `
+    
+    
+        
+        
+        
+
 })
 	.catch(function(error){
 	console.log('El error fué: ' + error);
