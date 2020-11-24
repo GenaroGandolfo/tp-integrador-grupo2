978646cb506131201b35f1bbf2836f344deb9b3c
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-24 20:19:43 -0300

    atajos

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', function(){
 
-    var apiKey  ='811c43dae475e925f89e17499fd11f5c';
-    var imageneslink ='https://image.tmdb.org/t/p/w500';
+   
 
     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`)
 	.then(function(response){
@@ -14,20 +13,19 @@ window.addEventListener('load', function(){
     var populares = document.querySelector ("#pop");
    
     
-    for (let index = 0; index < data.results.length; index++) {
-         var results = data.results[index];
+    for (let i = 0; i< data.results.length; i++) {
+         var results = data.results[i];
         
     }
 
 
-        populares.innerHTML += 
-        `
+        populares.innerHTML += `
         
-            <a href="movieDetails.html"> 
-            <div> <img src="${imageneslink}${results.poster_path}" alt="${results.title}"> </div> 
-            <div class="uk-position-center uk-panel"></div>
-            </a> 
-        
+            <a href="movieDetails.html">
+                <img src="${imageneslink}${results.poster_path}" alt="">
+                <div class="uk-position-center uk-panel"></div>
+            </a>     
+          
         `     
         
 
