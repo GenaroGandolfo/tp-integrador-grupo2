57b82952ab61dd655a62f675cede211bfb599ede
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-26 15:37:43 -0300

    nombre detales

--- js/genres.js
+++ js/genres.js
@@ -1,28 +1,22 @@
 window.addEventListener('load', function(){
+    
     var titulo = document.querySelector('.titulo');
     var peliculas = document.querySelector('.imagenesPelicula');
     var series = document.querySelector('.imagenesSerie');
     var tituloSeries = document.querySelector('.series');
     var carruselPeli = document.querySelector('#carruselPeli')
-   /*
-    //var queryString = location.search
-    var queryStringObj = new URLSearchParams (queryString)
-    var queryStringObjId = queryStringObj.get(`idShow`)
-    var stringTipo = queryStringObj.get(`tipo`)
-    var generoTipo = queryStringObj.get(`tipodegenero`)
-    console.log(queryStringObj.get(`id`))
-    var nombreGenero = queryStringObj.get(`nombredelgenero`)
-    */
+  
     let idEntrante = location.search;
     console.log(idEntrante);
     let idEntranteObj = new URLSearchParams(idEntrante);
     console.log(idEntranteObj);
     let id = idEntranteObj.get('id');
     console.log(id)
-    //Traer nombre
+
     let nombre = idEntranteObj.get('name');
     console.log(nombre)
-    //Fetch peliculas genero
+
+
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}`)
     .then(function(response){
         return response.json();
