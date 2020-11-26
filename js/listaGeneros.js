690a49f1571f1af54e35a9470e3ae001b773028d
Felipedearamburu <fdearamburu@udesa.edu.ar>
2020-11-26 03:37:56 -0300

    borre capturas, lista de generos, falta funcionalidad

---
+++ js/listaGeneros.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', function(){
+
+    fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`)
+    .then(function(response){
+        return response.json();
+    })
+
+
+    .then(function(data){
+        console.log(data);
+
+    let listaDeGeneros = document.querySelector('#listaDeGeneros');
+
+         for (let i = 0; i < data.genres.length; i++) {
+            
+            
+            listaDeGeneros.innerHTML +=
+
+            `<a href="detallesGeneros.html?id=${data.genres[i].id}&name=${data.genres[i].name}"><h2>${data.genres[i].name}</h2></a>`
+        }
+    })
+
+    .catch(function(error){
+        console.log('El error fué: ' + error); 
+    })
+
+})
