8595c54d5895c696251cee8b42676387b3fdf088
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-26 02:00:46 -0300

    rr

--- js/moviedetail.js
+++ js/moviedetail.js
@@ -5,7 +5,7 @@ window.addEventListener("load",function(){
     var queryStringObjId = queryStringObj.get(`idShow`)
     var stringTipo = queryStringObj.get(`tipo`)
     var generoTipo = queryStringObj.get(`tipodegenero`)
-    console.log(queryStringObj.get(`idShow`))
+    console.log(queryStringObj.get(`id`))
     var nombreGenero = queryStringObj.get(`nombredelgenero`)
 
     if (stringTipo == "pelicula") { 
@@ -19,20 +19,15 @@ fetch(`https://api.themoviedb.org/3/movie/${queryStringObjId}/reviews?api_key=${
         console.log(data);
 
     
-    var despeli= document.querySelector ('#movied');
-   
-    
-    for (let index = 0; index < data.results.length; index++) {
-        var results = data.results[index];
-    
+    var detalles= document.querySelector ('#detalle');
 
 
-        despeli.innerHTML += 
+        detalles.innerHTML += 
         `
         
         <div>
         <div class="titpeli">
-            <h2>${results.name}</h2>
+            <h2>${results.title}</h2>
         </div>
         <div class="despeli">
             <h4>Sinopsis</h4>
@@ -66,6 +61,47 @@ fetch(`https://api.themoviedb.org/3/movie/${queryStringObjId}/reviews?api_key=${
 	console.log('El error fué: ' + error); 
 })
 
+else if (stringTipo == "serie"){
+    fetch(`https://api.themoviedb.org/3/tv/${queryStringObjId}?api_key=c5fa76b40f5a5ea03c60140eade37d35&language=en-US`)
+    .then(function(response){
+    return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+
+        sectionDetalle.innerHTML += 
+        `
+
+        <div>
+        <div class="titpeli">
+            <h2>${results.title}</h2>
+        </div>
+        <div class="despeli">
+            <h4>Sinopsis</h4>
+            <p> ${results.overview}
+            </p>
+        </div>
+        <div class="despeli">
+            <h4>Elenco</h4>
+            <ul> 
+                <li>Robert Downey, Jr. </li>
+                <li>Terrence Howard</li>
+                <li>Jeff Bridges</li>
+                <li>Gwyneth Paltrow</li>
+                <li>Leslie Bibb</li>
+                <li> Shaun Toub</li>
+                <li> Faran Tahir</li>
+            </ul>
+        </div>
+
+
+        </div>
+        <div class="imagen">
+        <img src="${imageneslink}${results.poster_path}" alt="" width="450px">
+    </div>
+
+        `
+}
 
 
 
