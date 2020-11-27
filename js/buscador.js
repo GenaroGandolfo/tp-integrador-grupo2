0d6acde8a9205fbc07d617754b9cddc79090161b
GenaroGandolfo <ggandolfobrignoli@udesa.edu.ar>
2020-11-26 23:35:48 -0300

    .

--- js/buscador.js
+++ js/buscador.js
@@ -1,42 +1,38 @@
 window.addEventListener("load", function(){
 
-    let queryString = location.search;
-    let queryObject = new URLSearchParams(queryString)
-    let search = queryObject.get ('buscador');
-    console.log(search);
+    this.location.search
+    let niidea = location.search
     
-    var url = `https://image.tmdb.org/t/p/w500`
+    let queryString = new URLSearchParams(niidea)
     
-    let apiKey = "4bb2cde7041d18aaa0daf73019bbbaf9"
-    let link = `https://api.themoviedb.org/3/search/multi?api_key=4bb2cde7041d18aaa0daf73019bbbaf9&language=en-US&query=${search}&page=1&include_adult=false`
+    let loBuscado = queryString.get("buscador");
+
     
-    let resultados = document.querySelector('#resultados')
-    let titulo = document.querySelector(`.posta`)
-    
-    fetch(link)
-    .then(function (respuestas){
-        return respuestas.json()
-    })
-    
-    .then(function(data){
-        console.log(data);
-        let info = data.results;
-  
-        for (let i = 0; i < data.results.length; i++) {
-            const element = data.results[i];
-  
-            resultados.innerHTML += `<a href="detalles.html?type=${element.media_type}&idShow=${element.id}">
-                                <li>
-                                    <img src= "${url}${element.poster_path}" alt="${element.name}"/>
-                                        <div class="uk-position-center uk-panel"><h1></h1></div>
-                                </li>
-                            </a>`
-        
-        }
-        
-    })
-    .catch(error => console.log(error))
-    
+    fetch(`https://api.themoviedb.org/3/search/keyword?api_key=${apiKey}&query=${loBuscado}8&page=1`)
+        .then(function (response) {
+        return response.json();
+
+            }
+        )
+
+        .then(
+            function (data) {
+                let listadoResultados = document.querySelector("#resultadoB")
+
+                let resultadoBusqueda = data.results
+                console.log(resultadoBusqueda)
+
+
+                resultadoBusqueda.forEach(function (resultados) {
+                    listadoResultados.innerHTML +=
+                `
+                <a href=""><img src="${resultadoBusqueda.poster_path}" alt=""></a> 
+                <h2>$</h2>
+                `
+                });
+            }
+
+        )
     
     
     })
